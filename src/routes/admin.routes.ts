ee81edc391817064e76fdbc534313222ed5322f6
amarNrddn <124656036+amarNrddn@users.noreply.github.com>
2025-03-23 23:23:41 +0700

    admin  (#1)
    * update: manager aproved by
    
    * feat: update status procurment
    
    * Update manager.controller.ts
    
    * fixs conflix manager in procutment
    
    * fixed conflix in procurement
    
    * create confirm receipt procurement
    
    * update user

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,17 +1,22 @@
 import { Router } from 'express';
-import * as userController from '../controllers/user.controller';
+import * as userController from '../controllers/admin.controller';
 import { authenticate } from '../middlewares/auth.middleware';
 import { authorizeRole } from '../middlewares/role.middleware';
 import * as authController from '../controllers/auth.controller';
 
 const router = Router();
 
-router.post('/createakun', authenticate, authController.register)
-router.put(
-   '/:userId/role',
+router.post(
+   '/createakun',
    authenticate,
    authorizeRole(['ADMIN']),
-   userController.updateUserRole
+   authController.register)
+   
+router.put(
+   '/update/:userId',
+   authenticate,
+   authorizeRole(['ADMIN']),
+   userController.updateUser
 );
 
 export default router;
